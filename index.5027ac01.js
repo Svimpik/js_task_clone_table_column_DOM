72c41c4b6c8f7f52c5062e21b42c16ed2240a2a6
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-20 23:02:08 +0300

    make build

---
+++ index.5027ac01.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("table tr").forEach(function(e){e.cells[4].before(e.cells[1].cloneNode(!0))});
+//# sourceMappingURL=index.5027ac01.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+const table = document.querySelectorAll("table tr");
+table.forEach((row)=>{
+    row.cells[4].before(row.cells[1].cloneNode(true));
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
